6eba597dde4ed9de1130e2065bd1b165f3c4b854
mattiapesce <91664050+mattiapesce@users.noreply.github.com>
2021-12-02 07:39:38 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,21 @@
-
+#include "lib.h"
+char carattere (char a){
+  bool lettera ;
+  char scambio ;
+  if (((65<=int(a)) and (int(a) <=90)) or ((97<=int(a)) and (int(a)<=122))){
+    lettera = true;
+  }
+  else {
+    lettera = false ;
+  }
+  if (lettera and ((65 <= int(a)) and (int(a) <= 90 ))) {
+    scambio = int (a) +32;
+  }
+  else if (lettera and ((97<= int (a)) and (int (a) <= 122))) {
+    scambio = int(a) -32;
+  }
+  else {
+    scambio = "9" ;
+  }
+  return scambio ;
+}
